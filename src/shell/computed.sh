d6e74fdb61aad259baedbf69b6213a463f90a4d0
bleatingsheep <bleatingsheep@live.cn>
2025-05-28 16:00:21 +0000

    fix: multiple spaces in file name

--- src/shell/computed.sh
+++ src/shell/computed.sh
@@ -5,7 +5,7 @@ echo 'export default {
 find meme -type f -print0 | grep -z -i -E '\.(jpg|png|jfif|webp|gif|jpeg|bmp)$' | sort -z | while IFS= read -r -d '' file_name
 do
     echo -n '    ' >> ./static/scripts/config.js
-    echo $(jq -R -c . <<< "$file_name")',' >> ./static/scripts/config.js
+    echo "$(jq -R -c . <<< "$file_name")," >> ./static/scripts/config.js
     # echo ',' >> ./static/scripts/config.js
 done
 
